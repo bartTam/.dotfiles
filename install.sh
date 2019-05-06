f7e86b89004845b2e9f8ae37186de2fcc34f0652
bartTam <mrbart.tam@gmail.com>
2019-05-06 12:01:30 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ done
 
 # Install vim-plug for nvim
 # Taken from vim-plug github: https://github.com/junegunn/vim-plug
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+curl -sfLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Install tmux config files
